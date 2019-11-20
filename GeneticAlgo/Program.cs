54e457788ca62aa774ad5c35645653dcae2e439c
Stefan Bilic <0896795@hr.nl>
2019-11-20 11:02:09 +0100

    Add project files.

---
+++ GeneticAlgo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new DataReader();
+            reader.ReadData();
+        }
+    }
+}
